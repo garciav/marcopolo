3fd50c38c8793d95bacf5368e4d68f09739f0705
Abraham Garcìa Vidal <garciav.abraham@gmail.com>
2022-06-29 11:20:25 -0400

    Application config h2
    new data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+CREATE TABLE product (id long, name VARCHAR(255), url VARCHAR(255), description VARCHAR(255));
+INSERT INTO product (id, name, url, description) VALUES (1, 'notebook', 'myapp.com', 'producto de mi app');
